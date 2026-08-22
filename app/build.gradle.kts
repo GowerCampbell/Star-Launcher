d981c5bdf091f0c65e3b8343b6b32b0e532fe640
TheCodeCamper <119587144+GowerCampbell@users.noreply.github.com>
2026-08-22 17:53:39 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,4 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("com.google.code.gson:gson:2.10.1")
 }
