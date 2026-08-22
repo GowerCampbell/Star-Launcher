e7490837c12c0caf9baf81d81269078beb9540cc
TheCodeCamper <119587144+GowerCampbell@users.noreply.github.com>
2026-08-22 16:58:25 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.starlauncher"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "6.0"
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
@@ -34,9 +34,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("com.google.code.gson:gson:2.10.1")
 }
